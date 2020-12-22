09b3099d08025fcaab9fab425d72e3c4f7120c46
Your Name <you@example.com>
2020-12-22 20:15:24 +0530

    Added: exception handler to handle custom exception

--- src/main/java/com/blz/emppayroll/exception/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/blz/emppayroll/exception/EmployeePayrollExceptionHandler.java
@@ -25,4 +25,12 @@ public class EmployeePayrollExceptionHandler {
 		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request", errMesg);
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
 	}
+	
+	@ExceptionHandler(EmployeePayrollException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+			EmployeePayrollException exception) {
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request",
+														exception.getMessage());
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
 }
