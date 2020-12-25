bb318c5037536a8db4dd292e5127fb6ad179c5d8
Your Name <you@example.com>
2020-12-25 17:27:06 +0530

    Added: entity, table and specified column names

--- src/main/java/com/blz/emppayroll/model/EmployeePayrollData.java
+++ src/main/java/com/blz/emppayroll/model/EmployeePayrollData.java
@@ -3,25 +3,55 @@ package com.blz.emppayroll.model;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
 import com.blz.emppayroll.dto.EmployeePayrollDTO;
 
 import lombok.Data;
 
+@Entity
+@Table(name = "employee_payroll")
 public @Data class EmployeePayrollData {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "employee_id")
 	private int employeeId;
+	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "salary")
 	private long salary;
+	
+	@Column(name = "gender")
 	private String gender;
+	
+	@Column(name = "start_date")
 	private LocalDate startDate;
+	
+	@Column(name = "note")
 	private String note;
+	
+	@Column(name = "profile_pic")
 	private String profilePic;
+	
+	@ElementCollection
+	@CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+	@Column(name = "department")
 	private List<String> department;
 	
 	public EmployeePayrollData() {}
 	
-	public EmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
-		this.employeeId = empId;
+	public EmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
 		this.updateEmployeePayrollData(empPayrollDTO);
 	}
 
